422565d1dd70b036ec6b43f8e4acf3ed1d80c19b
ndtien142 <ndtien142@gmail.com>
2025-03-11 15:04:53 +0700

    config env and connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
 
 const app = express();
+
+app.use(morgan("dev"));
+// morgan("combined");
+// morgan("tiny");
+// morgan("short");
+// morgan("common");
+app.use(helmet());
+app.use(compression());
 
 // init middlewares
 
 // init db
+require("./dbs/init.mongodb");
+// const { countConnect } = require("./helpers/check.connect");
+// countConnect();
 
 // init routes
+app.get("/", (req, res, next) => {
+  return res.status(500).json({
+    message: "Hello world",
+  });
+});
 
 // handling error
 
